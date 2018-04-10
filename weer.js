79a6cb7b7e59e820c3880fd57ee66fb18f11dd41
Laurence van Es <Laurence@mbpvanlaurence.wireless.hhs.nl>
2018-04-10 11:35:58 +0200

    Bla bla test

--- weer.js
+++ weer.js
@@ -15,35 +15,40 @@ function tijd() {
     document.getElementById("tijd").innerHTML = hours+":"+mins;
 }
 
-// /**
-//  * Fetch API data
-//  */
-// function getAPIdata() {
-	
-// 	var url = "http://api.openweathermap.org/data/2.5/forecast";
-// 	var apiKey ="b56eb22343437e4b762ed7fafeac5691";
-// 	var city = "the%20Hague";
-
-// 	// construct request
-// 	var request = url + "?" + "appid=" + apiKey + "&" + "q=" + city;
-	
-// 	// get weather forecast
-// 	fetch(request)
-
-// 	// parse to JSON format
-// 	.then(function(response) {
-// 		return response.json();
-// 	})
-	
-// 	// render weather per day
-// 	.then(function(response) {
-
-// 	// 	// render weatherCondition
-// 	// 	onAPISucces(response);
-// 	// })
-// }
-
 setInterval(tijd, 100);
+
+function getAPIdata() {
+
+    var url = "http://api.openweathermap.org/data/2.5/forecast";
+    var apiKey ="b56eb22343437e4b762ed7fafeac5691";
+    var city = "the%20Hague";
+}
+
+fetch("http://api.openweathermap.org/data/2.5/weather?q=the%20hague&appid=b56eb22343437e4b762ed7fafeac5691&units=metric")
+    
+    // parse to JSON format 
+    .then(function(response) {
+        return response.json();
+    })
+    
+    // render weather per day
+    .then(function(response) {
+
+        console.log(response.main.temp);
+        document.getElementById('temperatuur').innerHTML = response.main.temp + "\xB0C"
+
+        console.log(response.weather[0].main);
+        document.getElementById('weertype').innerHTML = response.weather[0].main
+
+        console.log(response.wind.speed);
+        document.getElementById('humidity').innerHTML = response.main.humidity + "%"
+    })
+    
+    // catch error
+    .catch(function (error) {
+        // onAPIError();
+        console.error('Request failed', error);
+    });
 
 // init data stream
 getAPIdata();
